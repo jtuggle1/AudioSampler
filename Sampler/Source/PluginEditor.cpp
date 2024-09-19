ef5ff98886476dfe26e4494ad01d281c71d559eb
dev work <jtuggle1@asu.edu>
2024-09-19 10:11:00 -0700

    basic plugin allows to select and playback audio file at different speeds

---
+++ Sampler/Source/PluginEditor.cpp
@@ -0,0 +1,39 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+SamplerAudioProcessorEditor::SamplerAudioProcessorEditor (SamplerAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    mLoadButton.onClick = [&]() { audioProcessor.loadFile(); };
+    addAndMakeVisible(mLoadButton);
+    setSize (200, 200);
+}
+
+SamplerAudioProcessorEditor::~SamplerAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void SamplerAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void SamplerAudioProcessorEditor::resized()
+{
+    mLoadButton.setBounds(getWidth()/2 - 50, getHeight()/2 - 50, 100, 100);
+}
